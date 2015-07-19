f659f8133033628438b5912ac92337cafa1f19bd
Dawid Królak <taavit@gmail.com>
2015-07-20 01:45:17 +0200

    default request

--- app/start.php
+++ app/start.php
@@ -1,8 +1,20 @@
 <?php
 require (__DIR__.'/../vendor/autoload.php');
 
+$loop = React\EventLoop\Factory::create();
+$socket = new React\Socket\Server($loop);
+
+$http = new React\Http\Server($socket);
+$http->on('request', function ($request, $response) {
+    $response->writeHead(200, array('Content-Type' => 'text/plain'));
+    $response->end("Hello World!\n");
+});
+
+$port = isset($_ENV["PORT"]) ? $_ENV["PORT"] : 9090;
+$socket->listen($port);
+
 $device = new WindTunnel\DummyDevice\Device();
-$client = new WindTunnel\DummyDevice\TunnelClient('realm1');
+$client = new WindTunnel\DummyDevice\TunnelClient('realm1', $loop);
 
 $client->setDevice($device);
 
