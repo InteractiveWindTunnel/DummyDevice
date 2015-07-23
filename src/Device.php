a72822010c8bee4dfd6f14ce6dbfe4144bd19fcc
Dawid Królak <taavit@gmail.com>
2015-07-23 16:34:26 +0200

    added parameters handling

--- src/Device.php
+++ src/Device.php
@@ -1,10 +1,16 @@
 <?php
 namespace WindTunnel\DummyDevice;
 
+use Evenement\EventEmitterTrait;
+
 class Device
 {
+    use EventEmitterTrait;
+
     protected $powerOn = false;
     protected $lastValue = 0;
+    protected $fluctuation = 1;
+    protected $frequency = 1;
 
     public function powerOn()
     {
@@ -24,7 +30,35 @@ class Device
 
     public function getSinData()
     {
-        $this->lastValue += deg2rad(15);
-        return sin($this->lastValue) + (mt_rand(-10, 10) / 20);
+        $this->lastValue += deg2rad($this->frequency);
+        return sin($this->lastValue) + (mt_rand(-10, 10) / (int)$this->fluctuation);
+    }
+
+    public function setFluctuation($fluctuation)
+    {
+        $fluctuation = (int) $fluctuation;
+        if ($fluctuation === 0) {
+            $fluctuation = 1;
+        }
+        $this->fluctuation = $fluctuation;
+        $this->emit('fluctuation.changed', [$fluctuation]);
+        return $this;
+    }
+
+    public function setFrequency($frequency)
+    {
+        $this->frequency = $frequency;
+        $this->emit('frequency.changed', [$frequency]);
+        return $this;
+    }
+
+    public function getFrequency()
+    {
+        return $this->frequency;
+    }
+
+    public function getFluctuation()
+    {
+        return $this->fluctuation;
     }
 }
--- src/TunnelClient.php
+++ src/TunnelClient.php
@@ -13,13 +13,30 @@ class TunnelClient extends \Thruway\Peer\Client
      */
     public function onSessionStart($session, $transport)
     {
-        if (null === $session) {
-            return;
-        }
-        $session->register('com.windtunnel.powerOn', [$this->device, 'powerOn']);
-        $session->register('com.windtunnel.powerOff', [$this->device, 'powerOff']);
-        $session->register('com.windtunnel.powerState', [$this->device, 'powerState']);
         $device = $this->device;
+
+        $session->register('com.windtunnel.powerOn', [$this->device, 'power_on']);
+        $session->register('com.windtunnel.powerOff', [$this->device, 'power_off']);
+        $session->register('com.windtunnel.powerState', [$this->device, 'power_state']);
+        $session->register(
+            'com.windtunnel.setfrequency',
+            function ($value) use ($device) {
+                $device->setFrequency($value[0]);
+            }
+        );
+        $session->register(
+            'com.windtunnel.setfluctuation',
+            function ($value) use ($device) {
+                $device->setFluctuation($value[0]);
+            }
+        );
+
+        $this->device->on('fluctuation.changed', function ($fluctuation) use ($session, $device) {
+            $session->publish('com.windtunnel.fluctuation', [$fluctuation]);
+        });
+        $this->device->on('frequency.changed', function ($frequency) use ($session, $device) {
+            $session->publish('com.windtunnel.frequency', [$frequency]);
+        });
         $this->getLoop()->addPeriodicTimer(0.1, function () use ($session, $device) {
             $session->publish('com.windtunnel.data', [$device->getSinData()]);
         });
